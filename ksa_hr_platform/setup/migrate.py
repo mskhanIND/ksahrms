2fd8581088a0084534643a2c05375cabc2ec20a1
mskhanIND <mskhan.06@gmail.com>
2026-05-24 13:12:39 +0300

    Refresh clickable workspace on migrate

--- ksa_hr_platform/setup/migrate.py
+++ ksa_hr_platform/setup/migrate.py
@@ -1,8 +1,10 @@
 from ksa_hr_platform.setup.onboarding import create_onboarding_records
 from ksa_hr_platform.setup.install import create_default_services, create_roles
+from ksa_hr_platform.setup.workspace_cards import update_clickable_workspace
 
 
 def after_migrate():
     create_roles()
     create_default_services()
     create_onboarding_records()
+    update_clickable_workspace()
